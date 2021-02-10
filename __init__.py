b52b3531c43fdf151206f340e5fa7ac26e11cf5c
mikegaro <garcia.miguel.onate@gmail.com>
2021-02-09 23:11:10 -0600

    jeje

---
+++ __init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import os
+
+
+app = Flask(__name__, template_folder='./templates', static_folder='./static')
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+@app.route('/')
+def hello():
+    return render_template("index.html")
